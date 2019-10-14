35b82d0e9714832fe7dcf8fe659e2f9a89afd770
Malik Saifullah <maliksaifullah282@gmail.com>
2019-10-14 16:05:05 +0500

    ok

---
+++ javascript/myFile.js
@@ -0,0 +1,9 @@
+$(function() {
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true
+    });
+});
